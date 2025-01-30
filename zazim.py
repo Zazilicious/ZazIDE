8473d9b11687252ae53eea7decd29649a086452b
Lexi Murphy <63361013+Zazilicious@users.noreply.github.com>
2025-01-30 12:47:25 -0500

    Add files via upload

--- zazim.py
+++ zazim.py
@@ -45,6 +45,17 @@ def highlight_syntax(e=False):
             "keywords": ["NOP","nop", "STC","stc", "STD","std", "ADD","add", "LDCI","ldci", "LDD","ldd", "LDC","ldc", "PUSHI","pushi", "PUSHA","pusha", "LDB","ldb", "LDA","lda", "POUT","pout", "STA","sta", "STB","stb", "RSH","rsh", "HLT","hlt", "JMP","jmp", "SUB","sub", "JC","jc", "JZ","jz", "LDAI","ldai", "PIN","pin", "PSTAT","pstat", "CMP","cmp", "CMPI","cmpi", "AND","and", "ANDI","andi", "JNZ","jnz", "LDDI","lddi", "LDACD","ldacd", "LDBI","ldbi", "PUSHB","pushb", "JSR","jsr", "VLFB","vlfb", "VFBW","vfbw", "STACD","stacd", "ADDI","addi", "VFBBG","vfbbg", "AVAIL18","avail18", "AVAIL19","avail19", "AVAIL1A","avail1a", "AVAIL1B","avail1b", "AVAIL1C","avail1c", "AVAIL1D","avail1d", "AVAIL1E", "avail1e", "AVAIL1F","avail1f", "AVAIL20","avail20", "AVAIL21","avail21", "AVAIL22","avail22", "AVAIL23","avail23", "AVAIL24","avail24", "AVAIL25","avail25", "AVAIL26","avail26", "AVAIL27","avail27", "AIL28","avail28", "AVAIL29","avail29", "AVAIL2A","avail2a", "AVAIL2B","avail2b", "AVAIL2C","avail2c", "AVAIL2D","avail2d", "AVAIL2E","avail2e", "AVAIL2F","avail2f", "AVAIL30","avail30", "AVAIL31","avail31", "RTS","rts","PUSHC","PUSHD","SUBI","POPC","POPD"],
             "comment": r'\;.*',
             "string": r'".*?"'
+        },
+        
+        "c": {
+        "keywords": ["auto", "break", "case", "char", "const", "continue", "default", "do", "double", "else", "enum", "extern", "float", "for", "goto", "if", "inline", "int", "long", "register", "restrict", "return", "short", "signed", "sizeof", "static", "struct", "switch", "typedef", "union", "unsigned", "void", "volatile", "while"],
+        "comment": r'//.*|/\*[\s\S]*?\*/',
+        "string": r'".*?"|\'.*?\''
+        },
+        "cpp": {
+        "keywords": ["alignas", "alignof", "and", "and_eq", "asm", "auto", "bitand", "bitor", "bool", "break", "case", "catch", "char", "char16_t", "char32_t", "class", "const", "const_cast", "continue", "decltype", "default", "delete", "do", "double", "dynamic_cast", "else", "enum", "explicit", "export", "extern", "false", "final", "float", "for", "friend", "goto", "if", "inline", "int", "long", "mutable", "namespace", "new", "noexcept", "not", "not_eq", "nullptr", "operator", "or", "or_eq", "private", "protected", "public", "register", "reinterpret_cast", "return", "short", "signed", "sizeof", "static", "static_assert", "static_cast", "struct", "switch", "template", "this", "thread_local", "throw", "true", "try", "typedef", "typeid", "typename", "union", "unsigned", "using", "virtual", "void", "volatile", "wchar_t", "while"],
+        "comment": r'//.*|/\*[\s\S]*?\*/',
+        "string": r'".*?"|\'.*?\''
         }
     }
 
@@ -58,6 +69,16 @@ def highlight_syntax(e=False):
         lang = "javascript"
     elif file_extension == "p8":
         lang = "puls8"
+    elif file_extension == "c":
+        lang = "c"
+    elif file_extension == "cpp":
+        lang = "cpp"
+    elif file_extension == "cc":
+        lang = "cpp"
+    elif file_extension == "c++":
+        lang = "cpp"
+    elif file_extension == "cp":
+        lang = "cpp"
     else:
         lang = None
 
@@ -107,7 +128,7 @@ def open_file():
     m_text.delete("1.0", END)
     t_file = filedialog.askopenfilename(initialdir="~/", title="Open File",
                                         filetypes=(("Python", "*.py"), ("Lua Files", "*.lua"), ("Puls8 Files", "*.p8"), 
-                                                   ("JavaScript Files", "*.js"), ("All Files", "*.*")))
+                                                   ("JavaScript Files", "*.js"),("C Files", "*.c"),("C++ Files", "*.cpp"),("All Files", "*.*")))
     if t_file:
         global opened_name
         opened_name = t_file
@@ -124,7 +145,7 @@ def save_as_file(e=False):
     global opened_name
     t_file = filedialog.asksaveasfilename(defaultextension=".py", initialdir="~/", title="Save File",
                                          filetypes=(("Python Files", "*.py"), ("Lua Files", "*.lua"), ("Puls8 Files", "*.p8"), 
-                                                    ("JavaScript Files", "*.js"), ("All Files", "*.*")))
+                                                    ("JavaScript Files", "*.js"),("C Files", "*.c"),("C++ Files", "*.cpp"), ("All Files", "*.*")))
     opened_name = t_file
     with open(t_file, 'w') as f:
         f.write(m_text.get(1.0, END))
